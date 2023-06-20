261f17f69542d8d2e469bf99cbe547365c8c5bb4
MAX <grec99088@gmail.com>
2023-06-20 11:33:31 +0300

    update navigation

--- app/src/main/java/com/poklad/notesmvvm/MainActivity.kt
+++ app/src/main/java/com/poklad/notesmvvm/MainActivity.kt
@@ -2,8 +2,12 @@ package com.poklad.notesmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mToolbar: Toolbar
+    private lateinit var mNavController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
