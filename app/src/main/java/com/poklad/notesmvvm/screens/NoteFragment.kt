b90ae97bcae6d01e72403a171e2d69df040da859
MAX <grec99088@gmail.com>
2023-06-20 11:00:12 +0300

    Add navigation, layouts, fragments

---
+++ app/src/main/java/com/poklad/notesmvvm/screens/NoteFragment.kt
@@ -0,0 +1,25 @@
+package com.poklad.notesmvvm.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.poklad.notesmvvm.R
+
+class NoteFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+}
